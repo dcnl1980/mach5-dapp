f966b0285f0305a899160c6f86cb49125d78e02d
TheDev <cvsteenbergen@gmail.com>
2022-12-17 20:21:18 +0100

    update on buy form

--- src/components/TokenSale.vue
+++ src/components/TokenSale.vue
@@ -63,6 +63,183 @@
 
     <section id="icobox" class="p-6">
       <h2 class="text-4xl"> ICO BOX </h2>
+
+      <p>ContactAddress: {{ this.contractAddr }}</p>
+      <p>TotalSupply: {{ this.totalSupply }}</p>
+
+      <a id="tokensale"/>
+    <div class="text-3xl text-center w-full items-center justify-center pt-12 text-black">
+             Round <b>1</b> (<b>{{Math.round(this.price*1000)/1000}} CJ / {{this.curCoin.sym}}</b>)
+     
+      <p class="text-6xl font-extrabold text-black bg-clip-text bg-gradient-to-br from-yellow-300 to-yellow-600 pb-8 uppercase ">
+        Tokensale is live!</p>
+    </div>
+
+    <div v-show="is_paused">Tokensale is paused! Waiting for another round.</div>
+
+    
+    <section class="py-12 lg:px-10 mx-4" v-show="!is_paused">
+      
+      <!-- TOKENSALE WITHOUT WEB3 WALLET -->
+      <div v-if="!this.ethereum">
+        <div class="max-w-6xl mx-auto" x-data="{ qr: false }">
+          <label class="block font-medium font-bold text-4xl text-center">Send ETH or BNB to The Address
+            Below</label>
+          <div class="my-12 relative items-center">
+            <input type="text" id="address" readonly v-model="account"
+                   class="p-4 h-16 text-xs sm:text-2xl md:text-3xl text-gray-800 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full pr-12 border-gray-300 rounded-md">
+            <div class="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
+              <button type="button" class="px-2" @click="copyAddress">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="#000000" viewBox="0 0 48 48"
+                     stroke="currentColor">
+                  <path d="M35.389,9h-6.166V1.5c0-0.827-0.671-1.5-1.5-1.5H15.454c-0.375,0-0.736,0.142-1.013,0.395L4.543,9.457
+		c-0.31,0.285-0.487,0.688-0.487,1.106v19.882c0,0.826,0.671,1.5,1.5,1.5h6.166v7.5c0,0.826,0.671,1.5,1.5,1.5h22.166
+		c0.829,0,1.5-0.674,1.5-1.5V10.5C36.889,9.673,36.217,9,35.389,9z M14.318,4.576v5.574H8.229L14.318,4.576z M7.057,28.945V13.15
+		h8.761c0.829,0,1.5-0.672,1.5-1.5V3h8.905v6h-3.104c-0.375,0-0.735,0.143-1.013,0.396l-9.897,9.063
+		c-0.31,0.283-0.487,0.687-0.487,1.105v9.381H7.057L7.057,28.945z M21.984,13.576v5.572h-6.086L21.984,13.576z M33.889,37.945
+		H14.723V22.148h8.762c0.828,0,1.5-0.672,1.5-1.5V12h8.904V37.945z"/>
+                </svg>
+              </button>
+              <div class="relative" v-show="copiedTooltip">
+                <div
+                    class="absolute top-0 z-10 w-32 p-2 -mt-4 text-sm leading-tight text-white transform -translate-x-full -translate-y-full bg-yellow-500 rounded-lg shadow-lg">
+                  Address Copied to Clipboard
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div x-show="qr"
+               class="flex justify-center max-w-6xl bg-gradient-to-b from-yellow-400 to-yellow-300 shadow-lg rounded-lg p-6 mx-auto text-center">
+            <qr-code :text="account"></qr-code>
+          </div>
+
+
+          <h3 class="text-lg py-4">NEXTSHIB tokens will be sent after the tokensale has been ended. No need to claim. If
+            you use a wallet, you need to claim the tokens.</h3>
+
+
+        </div>
+
+      </div>
+      
+      <!-- WEB3 WALLET --> 
+      
+      <!-- DURING SALE -->
+      <div v-else-if="!is_paused&&(networkId==1)"
+           class="m-auto d-flex w-full py-6 px-4 rounded border shadow-2xl">
+        <!-- NO CLAIM -->
+        <div v-if="claimableAmount == 0">
+          <h2 class="block mb-4 px-6 font-bold text-left text-black text-xl">
+            Deposit below to purchase {{ this.tokenName }} ({{ this.tokenSymbol }})
+          </h2>
+          <div class="mt-10 flex flex-row justify-between px-6">
+            <h6 class="text-sm">Send</h6>
+            <h6 class="text-sm">Balance: {{ this.balance }}</h6>
+          </div>
+          <div class="mt-2 flex flex-row items-center justify-between mb-6 px-6">
+            <input
+                class="border appearance-none font-medium text-2xl py-1 p-3 rounded w-full  mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                type="text" placeholder="0" v-model="sendAmount">
+            <img src="../assets/img/icons/icon.png" class="w-6 h-6 -mt-3"/>
+            <label class="ml-2 font-semibold text-xl -mt-3">{{ this.curCoin.sym }}</label>
+      
+          </div>
+          <div class="mt-10 flex flex-row justify-between px-6">
+            <h6 class="text-sm">Receive</h6>
+            <h6 class="text-sm">Balance:{{ parseInt(this.nextBalance) }}</h6>
+          </div>
+
+          <div>
+
+
+  
+</div>
+
+
+          <div class="mt-2 flex flex-row items-center justify-between mb-6 px-6">
+            <input
+                class="border appearance-none font-medium text-2xl py-1 p-3 rounded w-full mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                type="text" placeholder="0" v-model="recvAmount">
+            <img src="../assets/img/icons/nextshib.png" class="w-6 h-6 -mt-3"/>
+            <label class="ml-2 font-semibold text-xl -mt-3">{{ this.tokenSymbol }}</label>
+
+          </div>
+          <button
+              class="mx-6 w-full bg-gradient-to-r from-gray-600 via-gray-800 to-gray-500  hover:from-yellow-300 hover:to-yellow-500 hover:text-black text-white font-bold py-4 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+              @click="buyToken" v-bind:disabled="sendAmount < 0.01 "
+          >
+            <span v-if="sendAmount == 0">Please enter the amount to BUY</span>
+            <span v-else-if="sendAmount < this.minBUY ">Minimum to BUY is {{ this.minBUY }} {{
+                this.curCoin.sym
+              }}</span>
+              <!-- Check if balance is sufficient -->
+              
+            <span v-else>BUY</span>
+          </button>
+        </div>
+        <!-- CLAIM -->
+        <div v-else class="bg-yellow-400 ">
+          <h1 class="text-6xl block mb-4 px-6 font-bold text-center text-black">🏅</h1>
+          <h2 class="text-2xl block mb-4 px-6 font-bold text-center text-black">
+            You are the owner of {{ claimableAmount.toFixed(0) }} {{ this.tokenSymbol }}
+          </h2>
+          <h4 class="text-lg text-black px-6 pb-6">You can claim these tokens after the tokensale is finished.</h4>
+        </div>
+      </div>
+      <!-- AFTER SALE -->
+      <div v-else-if="is_paused&&(networkId==1||networkId==56||networkId==137)&&claimableAmount>0"
+           class="m-auto d-flex lg:w-2/6 bg-yellow-400 p-4 rounded .shadow-2xl">
+        <h2 class="block mb-4 px-6 font-bold text-2xl text-left text-black">
+          <img src="../assets/img/logo/nextshib.png" height="120px" width="120px" class="mx-auto py-4"/>
+          CLAIM the purchased {{ this.tokenSymbol }}
+        </h2>
+        <div class="mt-10 flex flex-row justify-between px-6">
+          <h6 class="text-sm">Claimable {{ this.tokenSymbol }}</h6>
+          <h6 class="text-sm">Balance:{{this.nextBalance}}</h6>
+        </div>
+        <div class="mt-2 flex flex-row items-center justify-between mb-6 px-6">
+          <input class="appearance-none font-medium text-2xl py-1 rounded w-full  mb-3 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="0" v-model="claimableAmount" disabled>
+          <img src="#" height="20px" width="20px" />
+          <label class="ml-2 font-semibold text-xl">{{ this.tokenSymbol }}</label>
+        </div>
+        <button
+            class="inline-block px-8 py-3 bg-gradient-to-r from-gray-600 via-gray-800 to-gray-500  hover:from-yellow-300 hover:to-yellow-500 hover:text-black rounded-md text-md font-semibold text-white transition duration-200 shadow-md"
+            type="button"
+            @click="claimToken"
+        >
+          Claim
+        </button>
+      </div>
+      <!-- FALLBACK, NOT SUPPORTED NETWORK -->
+      <div v-else class="m-auto d-flex w-full bg-gray-800 p-4 rounded .shadow-2xl">
+        <h2 class="text-2xl block mb-4 px-6 font-bold text-left text-white" >
+          Tokensale is only available on these blockchain networks:
+        </h2>
+        <h4 class="text-md text-left px-6">Ethereum (mainnet)</h4>
+        <button
+            class="w-full bg-gradient-to-r from-gray-600 via-gray-800 to-gray-500  hover:from-yellow-300 hover:to-yellow-500 hover:text-black text-white font-bold py-4 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+            @click="switchNetwork('0x1')"
+        >
+          Switch to Ethereum
+        </button>
+      </div>
+
+      <!-- CONTRIBUTION INFORMATION -->
+      <div class="max-w-6xl mx-auto pt-6 pb-6 overflow-auto text-white" v-if="!is_paused">
+        Max contribution:
+        <span v-if="this.curCoin.sym == 'ETH'">1 ETH</span>
+        <br/>
+        Min contribution:
+        <span v-if="this.curCoin.sym == 'ETH'">0.01 ETH</span>
+      </div>
+  
+    </section>
+
+
     </section>
 
     </div>
@@ -117,6 +294,8 @@
       nextContractAddr: "",
       abi: PresaleJson.abi,
       tokenAbi: TokenJson.abi,
+      tokenName: 'ChainJoes',
+      tokenSymbol: 'CJ',
       alertShow: false,
       alertMsg: "",
       networkId: "1",
@@ -228,7 +407,7 @@
       this.getBalance();
       this.contractObj = new this.web3Obj.eth.Contract(this.abi, this.contractAddr);
       this.calcPrice();
-      //this.getSupply();
+      this.getSupply();
       //this.getPrice();
     },
 
@@ -242,8 +421,8 @@
     },
 
     async getSupply() {
-      await this.ContractObj.methods.totalSupply().call().then((result) => {
-          this.inCirculation = (result / 1e18).toFixed(0);
+      await this.tokenContractObj.methods.totalSupply().call().then((result) => {
+          this.totalSupply = (result / 1e18).toFixed(0);
       })
     },
 
@@ -266,9 +445,9 @@
       await this.contractObj.methods.price().call().then((res) => {
         this.price = 1 / (res / 1e18);
       })
-      await this.contractObj.methods.paused().call().then((res) => {
-        this.is_paused = res;
-      })
+      //await this.contractObj.methods.paused().call().then((res) => {
+      //  this.is_paused = res;
+      //})
       await this.contractObj.methods.claimable(this.account).call().then((res) => {
         this.claimableAmount = res / 1e18;
       })
